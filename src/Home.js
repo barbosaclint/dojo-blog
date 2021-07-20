0b8a97b4481df705230f9a4ae5f1cad83d7dafa1
barbosaclint <barbosaclint@gmail.com>
2021-07-20 17:48:51 +0800

    Lessons about react mapping, displaying of list

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,28 @@ import { useState } from "react"
 
 const Home = () => {
 
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25)
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome Party', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3 }
+    ])
+   
     const handleClick = () => {
-        setName('Luigi')
-        setAge(30)
+        
     }
 
     const handleClickAgain = (name) => {
-        console.log('hello ' + name)
+        
     }
 
     return (    
         <div className = "home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick = {handleClick} > Click me </button>
-
+            { blogs.map((blog) => (
+                <div className="blog-preview" key="{blog.id}">
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+            )) }    
         </div>
       );
 }
