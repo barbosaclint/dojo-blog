066620e71c2bcac7c3555edf3bf79b6251351fe5
barbosaclint <barbosaclint@gmail.com>
2021-07-25 13:22:10 +0800

    error handling

--- src/Home.js
+++ src/Home.js
@@ -11,23 +11,34 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect ( () => {
         fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setBlogs(data)
-            setIsPending(false)
-        })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not fetch')
+                }   
+                return res.json()             
+            })
+            .then(data => {
+                setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err.message)
+                setIsPending(false)
+            })
     }, []) 
 
 
     return (    
         <div className = "home">
+            { error && <div>{ error } </div> }
+
            {/* may blogs && kasi habang di pa nagloload hindi mag eerror */}
-           {isPending && <div> Loading ....... </div> } {/* pang Loading */}
+           { isPending && <div> Loading ....... </div> } {/* pang Loading */}
            { blogs && <BlogList blogs={blogs} title={"All Blogs!"} />}
         </div>
       );
